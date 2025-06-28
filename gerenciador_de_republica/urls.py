d7621c47c746aaaea71d86c0c26a56361dbcf363
Joao Victor <joaoor1516@gmail.com>
2025-06-28 17:37:12 -0300

    bootstrap

--- gerenciador_de_republica/urls.py
+++ gerenciador_de_republica/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('myapp.urls')),
+    path('auth/', include('usuarios.urls')),
     path('admin/', admin.site.urls),
 ]
